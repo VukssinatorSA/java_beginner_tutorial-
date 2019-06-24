5cfd0248b5ccc096dabb1a8f31f165a798900def
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-24 09:47:01 +0800

    initial commit

---
+++ src/MySweetProgram.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) { //methods, do something
+		boolean isPizzaDelicious = true; //false
+		byte b = 127; //10110110;
+		char c = 'Z'; //unicode
+		short s = 32767; //for small numbers
+		int i = 434343434343; //32 bit
+		long l = 5050505050505050L;   //64 bit, put an 'L' at the end 
+		
+		float f = 20.5f;  //32 bit
+		double d = 20.5;//64 bit
+		
+		public static final double PI = 3.14159; 
+		//like const in JS, but in Java, const is a reserved keyword
+		// if you have a global variable, try to make variable final so that 
+		//it can't be changed later
+	}
+}
+
+//variable creation:
+//1. declare
+//2. initialize
+
+//primitive types : boolean, byte, char, short, int ,long, float, double
+//objects - instance of a class (class the data type)
+
+//statically typed = variables are given data types up front (before compiling)
+//dynamically typed = variables do not have types. 
+
+//data type = the type of the data. How the computer interprets the data
+
+//literals - the value;
+//variable - 
+
+//argument: what you pass in to a method, when you call it
+//parameter: the variable to store arguments, when you define it
+
+//Static - you don't have to make an instance of something'
+//variable - stores some value
+//expression - evaluates to a value
+//operator - work on operands to produce a value
+//operand - the thing(s) the operator works on
